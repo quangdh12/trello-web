173e444c803bdfc22d81ead628b3b4d1564f6aaa
quangdh12 <quangdh.t63@gmail.com>
2023-11-24 09:13:47 +0700

    setup layout and project structure

---
+++ src/components/ModeSelect/index.jsx
@@ -0,0 +1,52 @@
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
+import Select from '@mui/material/Select';
+import LightModeOutlined from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlined from '@mui/icons-material/DarkModeOutlined';
+import SettingsBrightnessOutlined from '@mui/icons-material/SettingsBrightnessOutlined';
+import { useColorScheme } from '@mui/material';
+
+function ModeSelect() {
+    const { mode, setMode } = useColorScheme();
+
+    const handleChange = (event) => {
+        const selectedMode = event.target.value;
+        setMode(selectedMode);
+    };
+
+    return (
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <InputLabel id="label-select-light-dark-mode">Mode</InputLabel>
+            <Select
+                labelId="label-select-light-dark-mode"
+                id="select-light-dark-mode"
+                value={mode}
+                label="Mode"
+                onChange={handleChange}
+            >
+                <MenuItem value="light">
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                        <LightModeOutlined fontSize='small' />
+                        Light
+                    </Box>
+                </MenuItem>
+                <MenuItem value="dark">
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                        <DarkModeOutlined fontSize='small' />
+                        Dark
+                    </Box>
+                </MenuItem>
+                <MenuItem value="system">
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                        <SettingsBrightnessOutlined fontSize='small' />
+                        System
+                    </Box>
+                </MenuItem>
+            </Select>
+        </FormControl>
+    );
+}
+
+export default ModeSelect;
