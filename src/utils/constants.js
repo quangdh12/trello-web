b523b5db848945878b7bf30f8fb8e8b6f602a2ce
quangdh12 <quangdh.t63@gmail.com>
2023-12-25 15:48:43 +0700

    update BUILD_MODE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,15 @@
 let apiRoot = ''
 
-if (process.env.BUILD_MODE == 'dev') {
+console.log('import.meta.env::', import.meta.env);
+
+if (process.env.BUILD_MODE === 'dev') {
     apiRoot = 'http://localhost:8017'
 }
 
-if (process.env.BUILD_MODE == 'production') {
+if (process.env.BUILD_MODE === 'production') {
     apiRoot = 'https://trello-api-tgma.onrender.com'
 }
+console.log("🚀 ~ file: constants.js:9 ~ apiRoot:", apiRoot)
+
 export const API_ROOT = apiRoot
 
